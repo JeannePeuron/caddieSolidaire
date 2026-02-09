434e944a2abc4b3d6ff5bd9ef572b257f6535c2b
JeannePeuron <jeanne.peuron@gmail.com>
2026-02-09 15:12:29 +0100

    feat : refactorisation

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-const items = [
+const cart = [
   { name: "bananas", price: 1.2, quantity: 3 },
-  { name: "apples", price: 0.8, quantity: 6 },
-  { name: "carrots", price: 0.5, quantity: 10 },
-  { name: "tomatoes", price: 1.5, quantity: 4 },
-  { name: "zucchini", price: 1.1, quantity: 2 },
+  { name: "apples", price: 0.84, quantity: 6 },
+  { name: "carrots", price: 0.89, quantity: 10 },
+  { name: "tomatoes", price: 1.53, quantity: 4 },
+  { name: "zucchini", price: 1.12, quantity: 2 },
   { name: "oranges", price: 1.0, quantity: 8 },
 ];
 
 function calculateTotal(cart) {
-  // Faire la somme des prix x les quantités pour chaque objet.
+  let total = 0;
+
+  for (let i = 0; i < cart.length; i++) {
+    const price = cart[i].price;
+    const reducPrice = cart[i].price * 0.9;
+    const quantity = cart[i].quantity;
+
+    if (cart[i].quantity > 5) {
+      total += reducPrice * quantity;
+      //   console.log(total);
+    } else {
+      total += price * quantity;
+    }
+  }
+
+  return total.toFixed(2);
   // Si la quantité est supérieure à 5 il faut appliquer 10% sur le prix
   // Retourne un arrondi à deux décimales.
 }
-
-console.log("Coucou");
+console.log(calculateTotal(cart));
