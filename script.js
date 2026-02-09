6c5561dbef8957d3240e16dfbc624ba7f77075e5
JeannePeuron <jeanne.peuron@gmail.com>
2026-02-09 12:12:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const items = [
+  { name: "bananas", price: 1.2, quantity: 3 },
+  { name: "apples", price: 0.8, quantity: 6 },
+  { name: "carrots", price: 0.5, quantity: 10 },
+  { name: "tomatoes", price: 1.5, quantity: 4 },
+  { name: "zucchini", price: 1.1, quantity: 2 },
+  { name: "oranges", price: 1.0, quantity: 8 },
+];
+
+function calculateTotal(cart) {
+  // Faire la somme des prix x les quantités pour chaque objet.
+  // Si la quantité est supérieure à 5 il faut appliquer 10% sur le prix
+  // Retourne un arrondi à deux décimales.
+}
+
+console.log("Coucou");
